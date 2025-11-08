183810e22f43e01fe81366dfa79c6deeb9e47d5b
arfni <daniel-k@outlook.de>
2025-11-08 14:16:24 +0100

    added get

--- index.ts
+++ index.ts
@@ -9,3 +9,5 @@ export function shuffleArray(array) {
   }
   return array;
 }
+
+export const get = (from, selector) => selector.replace(/\[([^\[\]]*)\]/g, ".$1.").split(".").filter((t) => t !== "").reduce((prev, cur) => prev && prev[cur], from);
